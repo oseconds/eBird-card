b6aac7da4ad8f6d2305f193800b4b3d7068d9d0d
0seconds <123317581+oseconds@users.noreply.github.com>
2026-08-06 19:22:49 +0900

    Update generate-card.py

--- src/ebird_readme_card/generate-card.py
+++ src/ebird_readme_card/generate-card.py
@@ -91,7 +91,6 @@ def main():
         """
     
     right_details_svg += f"""
-    <text x="260" y="150" class="sub-label">Sighted:</text>
     <text x="315" y="150" class="sub-value" style="fill: #58a6ff;">{last_bird}</text>
     """
 
